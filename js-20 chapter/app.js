556d368ea7914f84746b3f565c1d1df5bfdc48f5
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-15 18:28:56 +0500

    pratices

--- js-20 chapter/app.js
+++ js-20 chapter/app.js
@@ -16,3 +16,12 @@ console.log(fruit.slice(1, 3));// slice ma mai agar (1, 3 ) tk dalo to wo 0 ko u
 
 
 console.log(fruit.indexOf("Ahmednadeem"));;// indexof ma gagr hum koi bhi chez string ma dalanga agar wo chez array ma exist krti ha to index number batada ga or wo chez array ma nahi ha to -1 agay ga
+
+const username = 'Ahmed nadeem';
+console.log(username.toUpperCase());
+console.log(username[2]);
+
+console.log(username.toLowerCase());
+
+// const city = prompt ("which city do you live");
+
