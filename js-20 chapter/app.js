91159e11ddc20c00aad50990d52f219b6cf0092d
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-15 18:15:17 +0500

    pratices

---
+++ js-20 chapter/app.js
@@ -0,0 +1,18 @@
+console.log("hello string method");
+
+// toLowerCase  book ma haga
+// toUpperCase  book ma haga
+// length  book ma haga
+// charAt  book ma haga
+// indexOf  book ma haga
+// lastindexof  book ma haga
+// slice  book ma haga
+// replace  book ma haga
+// replaceall  book ma haga
+// split
+
+const fruit =["apple","banana","orange","watermelon","grapes"];
+console.log(fruit.slice(1, 3));// slice ma mai agar (1, 3 ) tk dalo to wo 0 ko uara dy ga
+
+
+console.log(fruit.indexOf("Ahmednadeem"));;// indexof ma gagr hum koi bhi chez string ma dalanga agar wo chez array ma exist krti ha to index number batada ga or wo chez array ma nahi ha to -1 agay ga
